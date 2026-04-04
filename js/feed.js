695823313c80e2b312c9b3d56a2c447890af8f02
priscila-sureira <pspessoa02@gmail.com>
2026-04-04 14:39:31 -0300

    Animações no feed

---
+++ js/feed.js
@@ -0,0 +1,34 @@
+//Animação dos textos
+
+function animarLandingPage() {
+    const menu = document.querySelectorAll('.menu a');
+
+    menu.forEach((link, index) => {
+        setTimeout(() => {
+            link.style.opacity = '1';
+            link.style.transform = 'translateY(0)';
+        }, 500 + (index * 200));
+    });
+}
+
+window.addEventListener('load', animarLandingPage);
+
+//Zoom ao passar o mouse 
+
+function animarHover(botao) {
+    botao.addEventListener('mouseenter', () => {
+        gsap.to(botao, { scale: 1.1, duration: 0.3 });
+    });
+
+    botao.addEventListener('mouseleave', () => {
+        gsap.to(botao, { scale: 1, duration: 0.3 });
+    });
+}
+
+window.addEventListener('load', () => {
+    document.querySelectorAll('.btn-primary').forEach(animarHover);
+});
+
+// Animção
+
+
